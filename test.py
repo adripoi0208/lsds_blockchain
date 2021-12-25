4e4cf8c826e32823db457a308c7d3e9eb3f90275
thmazur <thomasmazur2@hotmail.com>
2021-12-25 16:46:29 +0100

    Flask exemple

---
+++ test.py
@@ -0,0 +1,28 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+print("Send a get request to helloworld by pressing any key.")
+input()
+response = requests.get(BASE + "helloworld")
+print(response.json())
+
+print("Send the post request to helloworld by pressing any key.")
+input()
+response = requests.post(BASE + "helloworld")
+print(response.json())
+
+print("Send a get request to paramexemple with tim as a parameter by pressing any key.")
+input()
+response = requests.get(BASE + "/paramexemple/tim")
+print(response.json())
+
+print("Send a put request to video by pressing any key.")
+input()
+response = requests.put(BASE + "video/1", {"likes":10})
+print(response.json())
+
+print("Send a get request to video by pressing any key.")
+input()
+response = requests.get(BASE + "video/1")
+print(response.json())
